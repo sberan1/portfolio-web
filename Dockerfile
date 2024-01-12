c25aeaeeff88346151d5a3401768e7dc6e32cd1b
sberan1 <stepan.beran@outlook.com>
2024-01-12 20:56:52 +0100

    ?????

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ FROM node:lts-slim
 RUN apt-get update -y && apt-get install -y openssl
 ENV NODE_ENV=development
 
+ARG SUPABASE_URL
+ARG SUPABASE_KEY
+ENV SUPABASE_URL=$SUPABASE_URL
+ENV SUPABASE_KEY=$SUPABASE_KEY
+
+# Rest of your Dockerfile...
 WORKDIR /app
 
 COPY package*.json ./
--- src/lib/server/supabaseClient.ts
+++ src/lib/server/supabaseClient.ts
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private';
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
